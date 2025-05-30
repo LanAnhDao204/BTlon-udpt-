562cffcc8dd98a4173239fda7e8256fef09c904b
LanAnhDao204 <22010262@st.phenikaa-uni.edu.vn>
2025-05-30 23:55:05 +0700

    .

--- BookStore/Backend/Database/dbconnection.js
+++ BookStore/Backend/Database/dbconnection.js
@@ -1,12 +1,12 @@
 // dbconnection.js
 
-const neo4j = require('neo4j-driver');
-require('dotenv').config();
+import neo4j from 'neo4j-driver';
+import dotenv from 'dotenv';
+dotenv.config();
 
-// Tạo kết nối đến Neo4j Aura (sử dụng SSL mặc định)
 const driver = neo4j.driver(
   process.env.NEO4J_URI,
   neo4j.auth.basic(process.env.NEO4J_USERNAME, process.env.NEO4J_PASSWORD)
 );
 
-module.exports = driver;
+export default driver;
